7c904a5d3eaf92caff712c55f9249c664509a615
ImErick <3r1ck.442@gmail.com>
2018-03-17 11:55:27 -0600

    finish delete all button

--- app/src/main/java/mx/iteso/erickgarcia/sesion9sqlitebd/MainActivity.java
+++ app/src/main/java/mx/iteso/erickgarcia/sesion9sqlitebd/MainActivity.java
@@ -3,10 +3,12 @@ package mx.iteso.erickgarcia.sesion9sqlitebd;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton addProduct, listProduct, clearAllProducts;
@@ -27,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
                 Fragment fragment = new FragmentAddUProduct();
                 fragmentTransaction.replace(R.id.main_fragment, fragment);
                 fragmentTransaction.commit();
+            }
+        });
+
+        listProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment fragment = new FragmentListProduct();
+                fragmentTransaction.replace(R.id.main_fragment, fragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        clearAllProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AdminSQL adminSQL = new AdminSQL(MainActivity.this, "test.db", null, 1);
+                SQLiteDatabase db = adminSQL.getWritableDatabase();
+                db.execSQL("DELETE FROM product");
+                db.close();
+                Toast.makeText(MainActivity.this, "Delete all information sucessfull", Toast.LENGTH_SHORT).show();
             }
         });
     }
