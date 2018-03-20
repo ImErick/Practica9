4dfa9923da158c1e00e8879d16edb16f479780d8
ImErick <3r1ck.442@gmail.com>
2018-03-19 18:01:31 -0600

    minor changes

--- app/src/main/java/mx/iteso/erickgarcia/sesion9sqlitebd/FragmentListProduct.java
+++ app/src/main/java/mx/iteso/erickgarcia/sesion9sqlitebd/FragmentListProduct.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -43,7 +44,8 @@ public class FragmentListProduct extends Fragment {
                 itemProduct.setProductPrice(cursor.getFloat(3));
                 itemProductArrayList.add(itemProduct);
             } while (cursor.moveToNext());
-        }
+        } else
+            Toast.makeText(getActivity(), "there is not data to show", Toast.LENGTH_SHORT).show();
 
         itemProductAdapter = new ItemProductAdapter(getActivity(), itemProductArrayList);
         recyclerView.setAdapter(itemProductAdapter);
